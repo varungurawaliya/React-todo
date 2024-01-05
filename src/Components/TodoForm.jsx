5fefa9a436479b4a6130b044471247719d1d2282
Varun Gurawaliya <varungurawaliya@gmail.com>
2024-01-05 15:21:19 +0530

    first commit

---
+++ src/Components/TodoForm.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useTodo } from "../Context/context";
+
+function TodoForm() {
+
+    const {addTodo} = useTodo()
+
+    const [todo, setTodo] = useState("")
+
+    const addTodoHandler = (e) => {
+        e.preventDefault()
+  
+        if (!todo) return
+  
+        addTodo({id: Date.now(), todo, completed: false})
+        setTodo("")
+      }
+  
+  
+
+    return (
+        <form  onSubmit = {addTodoHandler} className="flex">
+            <input
+                type="text"
+                placeholder="Write Todo..."
+                value={todo}
+                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                onChange={(e)=>setTodo(e.target.value)}
+            />
+            <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+                
+            >Add
+            </button>
+        </form>
+    );
+}
+
+export default TodoForm;
